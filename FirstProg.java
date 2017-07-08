dbb15f26dfaa0696b9838a6f7608297a045d8894
Jodi Walder <JoWalder303@gmail.com>
2017-07-08 11:13:47 -0600

    test exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Jodi Walder, am determined to learn how to code." );
+    System.out.println( "Today's date is 07/07/17" );
+
+    }
+  }
